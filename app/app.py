97457e392a69688f36ea2e71437fe7ead97d4a2b
Atalay Desktop <deveciatalay@gmail.com>
2024-05-23 12:56:04 +0300

    added website files

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ from menu import streamlit_menu
 
 def app():
     st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
-    st.switch_page("./pages./home.py")
+    st.switch_page("./pages/home.py")
 
 
 if __name__ == "__main__":
